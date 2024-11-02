609b6b8fd72bf44aec105a3f33d10025e51c66e5
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 22:15:54 +0600

    typescript is intersting

---
+++ built-in-type/index.ts
@@ -0,0 +1,14 @@
+{
+
+let userName:string = 'mehedi hasan';
+let userId:number = 24;
+let isActive:boolean = true ;
+
+
+function display() : void{
+    console.log('oi I am display');
+}
+display();
+
+    
+}
