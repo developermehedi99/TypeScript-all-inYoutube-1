8c1c66c73d750dc50c738d0d693696008d8e9823
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-03 00:03:30 +0600

    objects type

---
+++ user-define-type/objects-type.ts
@@ -0,0 +1,8 @@
+{
+    let developers : {position:string; skill:string; salary:number} = {
+        position:'frontend developer',
+        skill:'react',
+        salary:34999
+    }
+    console.log(developers);
+}
