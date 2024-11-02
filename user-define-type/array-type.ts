f1a58c3ea39947ae00cba9d223e79e59a8655872
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 22:59:52 +0600

    user define type

---
+++ user-define-type/array-type.ts
@@ -0,0 +1,6 @@
+{
+    let numbers: (number | string) [] = [34,53,63,74,84,23,'am','score'];
+    let members: Array<string>;
+    members=['js','react','typescript'];
+
+}
---
+++ user-define-type/union.ts
@@ -0,0 +1,11 @@
+
+let userId : string | number;
+
+userId = 'rabana';
+userId=445;
+
+function user( user:string | number  | boolean) {
+    console.log(user)
+}
+user('rana');
+user(43433);
