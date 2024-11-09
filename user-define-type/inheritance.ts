9bd1dca05d23c932967d36b3cf5efac37102a2b9
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-09 10:20:24 +0600

    inheritance

---
+++ user-define-type/inheritance.ts
@@ -0,0 +1,38 @@
+{
+    // inheritance
+    class User {
+        name:string;
+        age:number
+
+        constructor(name:string,age:number){
+            this.name = name,
+            this.age = age
+        }
+
+        display():void {
+            console.log(`hey, her is ${this.name} and her age ${this.age}`)
+        }
+    }
+
+    class Student extends User{
+        studentId: number
+
+        constructor(name:string, age:number, studentId:number){
+            super(name, age)
+            this.studentId= studentId;
+        }
+
+        displayStudent():void {
+            console.log(`she is a student. her name ${this.name},age ${this.age} and her student id is ${this.displayStudent}`);
+        }
+    }
+
+    const user1= new User('aisha',9);
+    user1.display();
+
+    const student1 = new Student('nurmoni', 15,52);
+    student1.displayStudent();
+
+
+    // 
+}
